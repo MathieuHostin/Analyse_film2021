d3795f16cfff221a3aa8ad64b85c68133c3ea516
MathieuHostin <71506432+MathieuHostin@users.noreply.github.com>
2022-01-04 16:42:39 +0100

    Create Webscrapping.py
    Création du dossier code + du programme de webscrapping

---
+++ Code/Webscrapping.py
@@ -0,0 +1,7 @@
+début = https://www.senscritique.com/liste/Recapitulatif_des_films_vus_en_2021/2914300#page-
+urls = []
+for i in range(13):
+    x = début + str(i + 1)
+    urls.append(x)
+
+print(urls)
